f6882e297d158d2760a83eecda56066d07889981
Raeez Lorgat <raeez@mit.edu>
2010-10-30 00:51:17 -0700

    bootstrap -> production + added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+  name='manhattan',
+  version='0.1',
+  long_description=__doc__,
+  packages=['cerberus', 'hermes', 'gaia', 'lib'],
+  include_package_data=True,
+  zip_safe=False,
+  install_requires=['Flask']
+     )
