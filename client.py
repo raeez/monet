9d08d1b532c51e86fb7e37bce2a7251de72837e7
Raeez Lorgat <raeez@mit.edu>
2011-01-09 00:23:48 +0200

    same syntax for debug launcher

--- client.py
+++ client.py
@@ -4,8 +4,8 @@ import lib.config
 lib.config.load('conf/local.json')
 lib.config.CONF['syslog'] = 'client'
 
-from client.app import client
+from client.app import client as app
 
 if __name__ == '__main__':
-  client.debug = True
-  client.run(port=5000)
+  app.debug = True
+  app.run(port=5000)
