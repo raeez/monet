efa2bdce2dc959a68fcbeb163c7279b422f553c5
Evan Morikawa <emmorikawa@gmail.com>
2011-02-18 16:50:33 -0500

    Started getting images to tile properly

--- client/static/js/memoize.js
+++ client/static/js/memoize.js
@@ -2,8 +2,12 @@
 var loginBoxActive = false;
 
 $(document).ready(function(){
+	defaultCanvasWidth = $('#photo_canvas_center').width();
+	
 	$('#canvas_login_form').show();
 	loginBoxActive = false;
+	
+	photoHFit();
 	
 	wrapResize();
 	
@@ -26,6 +30,47 @@ $(document).ready(function(){
 		$("#share_link_area").hide();
 	});
 	
+	hover_in_queue = [];
+	hover_out_queue = [];
+	original_width = 0;
+	last_edited = null;
+	hoverin = 0;
+	$(".photo_div").hover(function() {
+		hover_in_queue.push([$(this), $(this).width()]);
+		updateHoverQueue(hover_in_queue, hover_out_queue);
+		if (hoverin != 0) {
+			// Be sure to fix the last thing that didn't clear
+			$(last_edited).animate({
+				width: original_width,
+			}, 500);
+			
+			hoverin = 0
+		}
+		
+		hoverin = 1;
+		
+		diff = $(this).children('.photo').width() - $(this).width();
+		$('#photo_canvas_center').width($('#photo_canvas_center').width() + diff);
+		original_width = $(this).width();
+		last_edited = $(this);
+		
+		$(this).animate({
+				width: $(this).children('.photo').width(),
+			}, 500, function() {
+		});
+		
+	}, function() {
+		if (hoverin == 1) {
+			hoverin = 0;
+			diff = $(this).width() - original_width;
+			$(this).animate({
+					width: original_width,
+				}, 500, function() {
+				$('#photo_canvas_center').width($('#photo_canvas_center').width() - diff);
+			});
+		}
+	});
+	
 	/*
 	$("#canvas_login_div").mouseleave(function() {
 		loginBoxActive = false;
@@ -33,6 +78,68 @@ $(document).ready(function(){
 	});
 	*/
 });
+
+function updateHoverQueue(in_queue, out_queue) {
+}
+
+function photoHFit() {
+	var margin = 10;
+	var max_width = $("#photo_wrapper").width();
+	var row_accumulator = [];
+	var width_accumulator = 0;
+	
+	$(".photo").each(function(){
+		if (width_accumulator < max_width) {
+			width_accumulator += $(this).parent('.photo_div').width() + parseInt($(this).parent().css('margin-left')) + parseInt($(this).parent().css('margin-right'));
+			row_accumulator.push($(this).parent('.photo_div'));
+		} else {
+			resizePhotoDivs(row_accumulator, width_accumulator);
+			width_accumulator = 0 + $(this).parent('.photo_div').width() + parseInt($(this).parent().css('margin-left')) + parseInt($(this).parent().css('margin-right'));
+			row_accumulator = [];
+			row_accumulator.push($(this).parent('.photo_div'));
+		}
+	});
+	resizePhotoDivs(row_accumulator, width_accumulator);
+}
+
+function resizePhotoDivs(row_accumulator, default_width) {
+	console.log("row_accumulator: ");
+	console.log(row_accumulator);
+	console.log("default_width: " + default_width);
+	var i;
+	var overspill;
+	var length = row_accumulator.length;
+	
+	if (default_width > 955) {
+		overspill = default_width - 955;
+		console.log("overspill: " + overspill);
+		crop = Math.floor(overspill / length);
+		console.log("crop: " + crop);
+		
+		var width_accumulator = 0; 
+		console.log("lengthh: " + length);
+		for (i=0; i<length; i++) {
+			console.log(i);
+			photo_div = row_accumulator.pop();
+			console.log(photo_div);
+			
+			if (i == length - 1) {
+				console.log("LAST PHOTO");
+				// This means we're on the last photo. The last photo should take up the remaining slack.
+				$(photo_div).width(955 - width_accumulator - 10); // -10 because of the margin
+			} else {
+				// Else make the divs a bit smaller based on the crop size
+				newsize = $(photo_div).width() - crop;
+				console.log("original width: " + $(photo_div).width());
+				console.log("new width: " + newsize);
+				$(photo_div).width(newsize);
+			}
+			
+			width_accumulator += $(photo_div).width() + 10;
+			console.log("width_accumulator: " + width_accumulator);
+		}
+	}
+}
 
 function updateLoginBoxState() {
 	if (loginBoxActive == false) {
