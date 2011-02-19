4e21269aae73a171ba126ffcbfd34acd67541ed1
Raeez Lorgat <raeez@mit.edu>
2011-02-18 19:26:50 -0500

    added s3 upload script

---
+++ memoize/aws/s3.py
@@ -0,0 +1,21 @@
+IMAGE_BUCKET = 'memoize-images'
+SETTINGS_JSON = 'aws.json'
+
+def load_settings():
+  if globals().get('settings', None) is None:
+    with open(SETTINGS_JSON,'r') as f:
+      import json
+      return json.loads(f.read())
+
+settings = load_settings()
+
+def upload_image(filename, content):
+  from boto.s3.connection import S3Connection
+  from boto.s3.key import Key
+  conn = S3Connection(settings['access'], settings['secret'])
+  b = conn.get_bucket(IMAGE_BUCKET)
+  k = Key(b)
+  k.key = filename.split('/')[::-1][0]
+  k.set_metadata("Content-Type", 'images/jpeg')
+  k.set_contents_from_string(content)
+  k.set_acl("public-read") # fix, such that we don't expose everyone's photos
