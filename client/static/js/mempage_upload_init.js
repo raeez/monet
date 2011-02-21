d0f3735ffef160dd6e32c493da9f91a5e346fa28
Evan Morikawa <emmorikawa@gmail.com>
2011-02-21 18:28:27 -0500

    worked on homepage fixes

--- client/static/js/mempage_upload_init.js
+++ client/static/js/mempage_upload_init.js
@@ -51,16 +51,3 @@ $(function () {
         }
     });
 });
-/*
-        buildUploadRow: function (files, index) {
-            return $('<tr><td>' + files[index].name + '<\/td>' +
-                    '<td class="file_upload_progress"><div><\/div><\/td>' +
-                    '<td class="file_upload_cancel">' +
-                    '<button class="ui-state-default ui-corner-all" title="Cancel">' +
-                    '<span class="ui-icon ui-icon-cancel">Cancel<\/span>' +
-                    '<\/button><\/td><\/tr>');
-        },
-        buildDownloadRow: function (file) {
-            return $('<tr><td>' + file.name + '<\/td><\/tr>');
-        }
-        */
