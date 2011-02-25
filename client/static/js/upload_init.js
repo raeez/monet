e39900a056ec2c93b2dbf68c61fa2fae73951ecf
vramesh <vinayak.ramesh@gmail.com>
2011-02-25 18:47:06 -0500

    cleaned up pic uploading on landing page

--- client/static/js/upload_init.js
+++ client/static/js/upload_init.js
@@ -146,7 +146,6 @@ $("#canvas_file_upload").fileUploadUI({
             return $(
             '       <div class="upload_file_div">'+
             '           <div class="file_upload_content">'+
-			    //files[index].name + 
                             'uploading...' +
             '               <div class="file_upload_progress"><\/div>'+
             '               <div class="file_upload_cancel"><\/div>'+
@@ -158,11 +157,10 @@ $("#canvas_file_upload").fileUploadUI({
         buildDownloadRow: function (file) {
 	    alert(file);
             return $(
-            '       <div class="upload_file_div">'+
+            '       <div class="upload_file_div">'/*+
             '           <div class="file_upload_content">'+
-                            //file.name +
 			    file.name + 
-            '           <\/div>'+
+            '           <\/div>'*/+
             '           <div class="file_upload_preview"><img src="'+file.thumb_url+'"\/><\/div>'+
             '       <\/div>'
             );
