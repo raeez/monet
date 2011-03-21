576aa39dbdae974361210325d423e8142b87efac
Raeez Lorgat <raeez@mit.edu>
2011-03-20 21:41:50 -0400

    implemented + generified atomic (append|set); queries now accept str|objectid; updated memoize/upload/helpers.py to reflect new interface

--- lib/db/mongo/adapter.py
+++ lib/db/mongo/adapter.py
@@ -7,7 +7,31 @@ from lib.log import Logger
 syslog = Logger.system_log()
 from lib.db.adapter import Adapter, AdapterConnectionError
 from lib.db.mongo.config import HOST, SAFE, REPLICATE_MIN
+from lib.db.objectid import ObjectId
 
+def enforce_valid_id(_id):
+  """
+  ensure that the supplied _id is in a form mongo can understand
+  """
+  if (not _id):
+    return _id
+
+  if isinstance(_id, basestring):
+    return ObjectId(_id)
+
+  assert isinstance(_id, ObjectId), "Invalid _id object for query %r\nMust be string or ObjectId" % params
+
+  return _id
+  
+def map_id(params):
+  """
+  ensure that object id's are being passed in a form mongo can understand
+  """
+  _id = params.get("_id", False)
+  if _id:
+    params["_id"] = enforce_valid_id(_id)
+  return params
+  
 class MongoAdapter(Adapter):
   """Abstraction and management of MongoDB instances"""
 
@@ -28,10 +52,10 @@ class MongoAdapter(Adapter):
     syslog['db'].debug("Added a document to a mongo database")
 
   def find(self, collection, params={}):
-    return list(self.db[collection].find(params))
+    return list(self.db[collection].find(map_id(params)))
 
   def find_one(self, collection, params={}):
-    return self.db[collection].find_one(params)
+    return self.db[collection].find_one(map_id(params))
 
   def group_find(self, group, params={}):
     results = []
@@ -49,19 +73,24 @@ class MongoAdapter(Adapter):
     return results
 
   def delete(self, collection, _id):
-    self.db[collection].remove({"_id" : _id})
+    self.db[collection].remove({"_id" : enforce_valid_id(_id)})
 
   def group_delete(self, group, _id):
     for collection in self.db.collection_names():
       if group in collection:
-        self.delete(collection, _id)
+        self.delete(collection, enforce_valid_id(_id))
     return
 
-  def atomic_append(self, collection, _id, item):
-    super(MongoAdapter, self).atomic_append(collection, _id, item)
-    self.db[collection].update({ "_id" : _id },
+  def atomic_append(self, collection, query, item):
+    super(MongoAdapter, self).atomic_append(collection, map_id(query), item)
+    self.db[collection].update(map_id(query),
                                { "$push" : item })
 
+  def atomic_set(self, collection, query, item):
+    super(MongoAdapter, self).atomic_set(collection, map_id(query), item)
+    self.db[collection].update(map_id(query),
+                               { "$set" : item })
+  
   def save(self, collection, document):
     super(MongoAdapter, self).insert(collection, document)
     self.db[collection].save(document, safe=SAFE, w=REPLICATE_MIN)
