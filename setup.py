b2c34d77e3445c7b812f1d1bc79e01bedfe2ed10
Raeez Lorgat <raeez@mit.edu>
2011-03-25 18:54:19 -0400

    updated package specification

--- setup.py
+++ setup.py
@@ -4,14 +4,22 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'monet',
-  version = '0.18',
+  version = '0.19',
   packages = find_packages(),
   include_package_data = True,
   zip_safe = False,
 
   author = 'Raeez Lorgat',
   author_email = 'raeez@mit.edu',
-  description = 'monet photo sharing',
+  description = 'monet live streaming',
 
-  install_requires=['Flask>=0.6', 'pymongo', 'py-bcrypt', 'gunicorn', 'gevent', 'eventlet', 'celery', 'flask-uploads', 'pyzmq']
+  install_requires=['Flask>=0.6',
+                    'pymongo',
+                    'py-bcrypt',
+                    'gunicorn',
+                    'gevent',
+                    'eventlet',
+                    'celery',
+                    'flask-uploads',
+                    'pyzmq']
 )
